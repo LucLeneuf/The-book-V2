e308497cabe6054681d6347cd2f04a6715400485
LucLeneuf <80699609+LucLeneuf@users.noreply.github.com>
2021-06-05 23:54:35 +0200

    Work in progress : visual page

---
+++ pages/visual.php
@@ -0,0 +1,74 @@
+<?php require "../require/header.php"; ?>
+
+<div class="margin_one">
+
+	<div class="visual">
+
+                <div class="visual__item">
+
+							<p class="visual__item__text_mobile">
+                                <span style="color:white;" >WEBSITE</span><br> 
+							    MODEL
+                            </p>
+					
+
+						<div class="visual__item__thumbnail">
+							<a href="into_visual/website_model.php">
+								<img src="../assets/images/thumbnail_website.jpg">
+
+					   			<p class="visual__item__thumbnail__text_pc"> 
+                                    <span style="color:white;">WEBSITE</span><br> 
+							        MODEL
+                                </p>
+							</a>
+						</div>
+				</div>
+
+
+
+				<div class="Magazine_mockup">
+
+				   		 <div class="grand marge mini">
+							<p> MAGAZINE </p> 
+							<p class="gris3"> MOCKUP </p>
+						</div>
+
+						<div class="miniature">
+							<a href="magazine_mockup.html">
+								<img src="img/Magazine_miniature.jpg">
+								
+								<div class="hover">
+					   				<p> MAGAZINE MOCKUP </p>
+					   			</div>
+					   		</a>
+						</div>
+				</div>
+
+
+				<div class="Digital_coloring">
+
+						<div class="grand marge mini">
+							<p>DIGITAL</p> 
+							<p class="gris3">COLORING</p>
+						</div>
+
+						<div class="miniature">
+							<a href="digital_coloring.html">
+								<img src="img/minia_kugo.jpg">
+
+								<div class="hover">
+					   				<p> DIGITAL COLORING </p>
+					   			</div> 
+							</a>
+						</div>
+				</div>
+
+				
+			
+
+
+	</div>
+
+</div>
+
+<?php require "../require/footer.php"; ?>
