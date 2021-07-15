0b0b5ecf113e6e80677bc663ae3f38ddf3bdb66d
luc <80699609+LucLeneuf@users.noreply.github.com>
2021-07-15 16:10:52 +0200

    contact css

--- pages/contact.php
+++ pages/contact.php
@@ -1,8 +1,14 @@
 <?php require "../require/header.php"; ?>
 
+
+
     <form action="contact_handler.php" method="post">
 
+        <p class="article__text__title big"> CONTACT ME </p>
+
         <div class="field">
+
+            <div class="field__info">
                 <div class="name">
                     <label for="name">Name</label>
                     <input type="text" id="name" name="name" required>
@@ -12,15 +18,15 @@
                     <label for="email">Email</label>
                     <input type="email" id="email" name="email" required >
                 </div>
-
+            </div>
                 
                 <div class="content">
-                    <label for="content">Content</label>
-                    <input type="textarea" id="content" name="content" required >
+                    <label for="message">Message</label>
+                    <textarea id="content" name="message" cols="40" rows="6" required></textarea>
                 </div>
         </div>
 
-        <input type="submit" name="send">
+        <input type="submit" name="send" value="Send">
         
     </form>
 
--- pages/contact_handler.php
+++ pages/contact_handler.php
@@ -1,22 +1,22 @@
 <?php
-  if(!empty($_POST["send"])&& isset($_POST["name"]) && !empty($_POST["name"]) && isset($_POST["email"]) && !empty($_POST["email"]) && isset($_POST["content"]) && !empty($_POST["content"])) {
+  if(!empty($_POST["send"])&& isset($_POST["name"]) && !empty($_POST["name"]) && isset($_POST["email"]) && !empty($_POST["email"]) && isset($_POST["message"]) && !empty($_POST["message"])) {
     $name = strip_tags($_POST["name"]) ;
     $email = strip_tags($_POST["email"]);
-    $message = strip_tags($_POST["content"]);
+    $message = strip_tags($_POST["message"]);
 
 
     $toEmail = "luc.leneuf@hotmail.fr";
     
-    $mailHeaders = " message portfolio - From: " . $name . " (". $email .")";
+    $mailHeaders = " message book - From: " . $name . " (". $email .")";
     mail($toEmail,$mailHeaders,$message);
 
-    echo '<body onLoad="alert(\'votre message a bien été envoyé\')">';
+    echo '<body onLoad="alert(\'Your message has been sent.\')">';
     echo '<meta http-equiv="refresh" content="0;URL=home.php">';
   
   }
     else{
       
-      echo '<body onLoad="alert(\'un champ est manquant\')">';
+      echo '<body onLoad="alert(\'One or more fields are missing.\')">';
       echo '<meta http-equiv="refresh" content="0;URL=contact.php">';
     }
 ?>
